7962ac8e2d3ee83a68d363a00f4a8c2f95500421
JoyEzenwoke <joyezenwoke206@gmail.com>
2025-11-05 09:47:12 -1100

    Add Render domain to ALLOWED_HOSTS

--- citizens_api/settings.py
+++ citizens_api/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-ja@37ewcv=g#!r^gqoz+r1(2#p9%nq5mvjv(+#u@506c82nb$$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-country-api-3a9k.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
